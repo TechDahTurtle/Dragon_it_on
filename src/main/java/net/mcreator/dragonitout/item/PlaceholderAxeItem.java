f350b584ccec8039a16dd248909d768d93ef64be
TechDahTurtle <atwst@AlphaOne-Main-Adam.home>
2020-12-12 09:14:00 +0000

    Updated Placeholder componants

---
+++ src/main/java/net/mcreator/dragonitout/item/PlaceholderAxeItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.dragonitout.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import net.mcreator.dragonitout.DragonItOutModElements;
+
+@DragonItOutModElements.ModElement.Tag
+public class PlaceholderAxeItem extends DragonItOutModElements.ModElement {
+	@ObjectHolder("dragon_it_out:placeholder_axe")
+	public static final Item block = null;
+	public PlaceholderAxeItem(DragonItOutModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 250;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 0f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(PlaceholderIngotItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("placeholder_axe"));
+	}
+}
---
+++ src/main/java/net/mcreator/dragonitout/item/PlaceholderIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.dragonitout.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.dragonitout.DragonItOutModElements;
+
+@DragonItOutModElements.ModElement.Tag
+public class PlaceholderIngotItem extends DragonItOutModElements.ModElement {
+	@ObjectHolder("dragon_it_out:placeholder_ingot")
+	public static final Item block = null;
+	public PlaceholderIngotItem(DragonItOutModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("placeholder_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
